7e95c7ae4c1ecbb370f31b00d55a5f738ac743da
Daniel Burnley <dan@danops.uk>
2017-02-09 21:17:25 +0000

    Add error message

--- src/DI.py
+++ src/DI.py
@@ -12,6 +12,6 @@ class DI:
             return cls()
         parameters = [param if not param.annotation == Parameter.empty else None for param in signature(cls.__init__).parameters.values()][1:]
         if parameters.count(None) > 0:
-            raise NonAnnotatedDependencyError
+            raise NonAnnotatedDependencyError("Could not resolve all dependencies - Missing type annotaion")
         to_inject = [DI.get_instance_of(param.annotation) for param in parameters]
         return cls(*to_inject)
